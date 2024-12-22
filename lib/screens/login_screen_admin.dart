812e7c91c3ca3f060140811c593a89ca44383fbb
Mudit200408 <muditpurohit2004.pm@gmail.com>
2024-12-22 23:06:00 +0530

    Cleanup Create-Account screens, Email and Password textfields and improve the login UI

--- lib/screens/login_screen_admin.dart
+++ lib/screens/login_screen_admin.dart
@@ -1,12 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:post_ace/screens/signup_screen.dart';
 import 'package:post_ace/services/auth_service.dart';
 import 'home_screen.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:flutter/painting.dart';
-// TODO: Remove this import when Firebase is setup
-// import 'package:atharva_coe/services/auth_service.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -29,7 +25,7 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    void _login() async {
+    void login() async {
       String Email = _email.text;
       String Password = _password.text;
 
@@ -62,161 +58,80 @@ class _LoginScreenState extends State<LoginScreen> {
       }
     }
 
-    // TODO: Firebase Auth setup
-    // 1. Initialize Firebase Auth
-    // 2. Implement email validation for @atharvacoe.ac.in
-    // 3. Setup Google Sign In for admin authentication
-
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(height: 40),
-                const Text(
-                  'WELCOME',
-                  style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                // Login illustration
-                SvgPicture.asset(
-                  'assets/illustrations/login_prof_illus.svg',
-                  height: 200,
-                ),
-                const SizedBox(height: 40),
-                TextField(
-                  controller: _email,
-                  decoration: InputDecoration(
-                    prefixIcon: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: SvgPicture.asset(
-                        'assets/icons/lock.svg',
-                        colorFilter: const ColorFilter.mode(
-                          Colors.black,
-                          BlendMode.srcIn,
-                        ),
-                      ),
-                    ),
-                    hintText: 'E-Mail',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(13),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16),
-                TextField(
-                  controller: _password,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    prefixIcon: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: SvgPicture.asset(
-                        'assets/icons/key.svg',
-                        colorFilter: const ColorFilter.mode(
-                          Colors.black,
-                          BlendMode.srcIn,
-                        ),
-                      ),
-                    ),
-                    hintText: 'Password',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(13),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 24),
-                SizedBox(
-                  width: 180,
-                  height: 40,
-                  child: ElevatedButton(
-                    onPressed: _login,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF6C63FF),
-                      foregroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                    ),
-                    child: const Text('Login'),
-                  ),
-                ),
-                SizedBox(
-                  width: 180,
-                  height: 40,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // TODO: Implement email validation
-                      // if (email.endsWith('@atharvacoe.ac.in')) {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const SignIn(),
-                        ),
-                      );
-                      // }
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF6C63FF),
-                      foregroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                    ),
-                    child: const Text('create account'),
-                  ),
-                ),
-                const SizedBox(height: 26),
-                SizedBox(
-                  width: 250,
-                  height: 50,
-                  child: OutlinedButton.icon(
-                    onPressed: () async {
-                      final UserCredential? userCred = await _auth.loginWithGoggle();
-                      if (userCred != null && context.mounted) {
-                        final user = userCred.user;
-                        print("User Successfully logged in");
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                            content: Text('Login Successful!'),
-                            backgroundColor: Colors.green,
-                            duration: Duration(seconds: 2),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(height: 40),
+            const Text(
+              'WELCOME',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 120),
+             Center(
+                child: SingleChildScrollView(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          // Login illustration
+                          SvgPicture.asset(
+                            'assets/illustrations/login_prof_illus.svg',
+                            height: 300,
                           ),
-                        );
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => HomeScreen(
-                              isAdmin: true,
-                              userName: user?.displayName ?? 'User',
-                              profileUrl: user?.photoURL ?? '',
+                          const SizedBox(height: 40),
+                          SizedBox(
+                            width: 250,
+                            height: 50,
+                            child: OutlinedButton.icon(
+                              onPressed: () async {
+                                final UserCredential? userCred =
+                                    await _auth.loginWithGoggle();
+                                if (userCred != null && context.mounted) {
+                                  final user = userCred.user;
+                                  print("User Successfully logged in");
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                      content: Text('Login Successful!'),
+                                      backgroundColor: Colors.green,
+                                      duration: Duration(seconds: 2),
+                                    ),
+                                  );
+                                  Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => HomeScreen(
+                                        isAdmin: true,
+                                        userName: user?.displayName ?? 'User',
+                                        profileUrl: user?.photoURL ?? '',
+                                      ),
+                                    ),
+                                  );
+                                }
+                              },
+                              style: OutlinedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50),
+                                ),
+                              ),
+                              icon: SvgPicture.asset(
+                                'assets/icons/google_logo.svg',
+                                height: 24,
+                              ),
+                              label: const Text('Sign-in with Google'),
                             ),
                           ),
-                        );
-                      }
-                    },
-                    style: OutlinedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                    ),
-                    icon: SvgPicture.asset(
-                      'assets/icons/google_logo.svg',
-                      height: 24,
-                    ),
-                    label: const Text('Sign-in with Google'),
+                        ]),
                   ),
                 ),
-              ],
-            ),
-          ),
+              ),
+          ],
         ),
       ),
     );
--- lib/screens/login_screen_student.dart
+++ lib/screens/login_screen_student.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:post_ace/screens/signup_screen_student.dart';
 import 'home_screen.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:flutter/painting.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:post_ace/services/auth_service.dart';
-import 'registration/gender_selection_screen.dart';
 
 class LoginScreenStudent extends StatefulWidget {
   const LoginScreenStudent({super.key});
@@ -54,7 +51,11 @@ class _LoginScreenStudentState extends State<LoginScreenStudent> {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(
-          builder: (context) => const HomeScreen(isAdmin: false, userName:  'Student',profileUrl: '',),
+          builder: (context) => const HomeScreen(
+            isAdmin: false,
+            userName: 'Student',
+            profileUrl: '',
+          ),
         ),
       );
     }
@@ -64,165 +65,99 @@ class _LoginScreenStudentState extends State<LoginScreenStudent> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(height: 40),
-                const Text(
-                  'WELCOME',
-                  style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                SvgPicture.asset(
-                  'assets/illustrations/login_student_illus.svg',
-                  height: 200,
-                ),
-                const SizedBox(height: 40),
-                TextField(
-                  controller: _email,
-                  decoration: InputDecoration(
-                    prefixIcon: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: SvgPicture.asset(
-                        'assets/icons/lock.svg',
-                        colorFilter: const ColorFilter.mode(
-                          Colors.black,
-                          BlendMode.srcIn,
-                        ),
-                      ),
-                    ),
-                    hintText: 'E-Mail',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(13),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16),
-                TextField(
-                  controller: _password,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    prefixIcon: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: SvgPicture.asset(
-                        'assets/icons/key.svg',
-                        colorFilter: const ColorFilter.mode(
-                          Colors.black,
-                          BlendMode.srcIn,
-                        ),
-                      ),
-                    ),
-                    hintText: 'Password',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(13),
-                    ),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const SignInStudent(),
-                          ),
-                        );
-                      },
-                      style: TextButton.styleFrom(
-                        foregroundColor: Colors.purple,
-                      ),
-                      child: const Text('Create Account?'),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 10),
-                SizedBox(
-                  width: 180,
-                  height: 40,
-                  child: ElevatedButton(
-                    onPressed: _login,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF6C63FF),
-                      foregroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                    ),
-                    child: const Text('Login'),
-                  ),
-                ),
-                const SizedBox(height: 26),
-                SizedBox(
-                  width: 250,
-                  height: 50,
-                  child: OutlinedButton.icon(
-                    onPressed: () async {
-                      try {
-                        final UserCredential? userCred = await _auth.loginWithGoggle();
-                        if (userCred != null && context.mounted) {
-                          final User? user = await userCred.user;
-                          final String? email = user?.email;
-                          
-                          if (email != null && email.endsWith('@atharvacoe.ac.in')) {
-                            print("User Successfully logged in");
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                content: Text('Login Successful!'),
-                                backgroundColor: Colors.green,
-                                duration: Duration(seconds: 2),
-                              ),
-                            );
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => HomeScreen(
-                                  isAdmin: false,
-                                  userName: user?.displayName ?? 'Student',
-                                  profileUrl: user?.photoURL ?? '',
-                                ),
-                              ),
-                            );
-                          } else {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                content: Text('Please use your college email address'),
-                                backgroundColor: Colors.red,
-                                duration: Duration(seconds: 2),
-                              ),
-                            );
-                          }
-                        }
-                      } catch (e) {
-                        print("Error during Google Sign In: $e");
-                      }
-                    },
-                    style: OutlinedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                    ),
-                    icon: SvgPicture.asset(
-                      'assets/icons/google_logo.svg',
-                      height: 24,
-                    ),
-                    label: const Text('Sign-in with Google'),
-                  ),
-                ),
-                const SizedBox(height: 26),
-              ],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start, 
+          children: [
+          const SizedBox(height: 40),
+          const Text(
+            'WELCOME',
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
             ),
           ),
-        ),
+          const SizedBox(
+            height: 100,
+          ),
+          Center(
+            child: SingleChildScrollView(
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset(
+                      'assets/illustrations/login_student_illus.svg',
+                      height: 250,
+                    ),
+                    const SizedBox(height: 50),
+                    SizedBox(
+                      width: 250,
+                      height: 50,
+                      child: OutlinedButton.icon(
+                        onPressed: () async {
+                          try {
+                            final UserCredential? userCred =
+                                await _auth.loginWithGoggle();
+                            if (userCred != null && context.mounted) {
+                              final User? user = userCred.user;
+                              final String? email = user?.email;
+
+                              if (email != null &&
+                                  email.endsWith('@atharvacoe.ac.in')) {
+                                print("User Successfully logged in");
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                    content: Text('Login Successful!'),
+                                    backgroundColor: Colors.green,
+                                    duration: Duration(seconds: 2),
+                                  ),
+                                );
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => HomeScreen(
+                                      isAdmin: false,
+                                      userName: user?.displayName ?? 'Student',
+                                      profileUrl: user?.photoURL ?? '',
+                                    ),
+                                  ),
+                                );
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                    content: Text(
+                                        'Please use your college email address'),
+                                    backgroundColor: Colors.red,
+                                    duration: Duration(seconds: 2),
+                                  ),
+                                );
+                              }
+                            }
+                          } catch (e) {
+                            print("Error during Google Sign In: $e");
+                          }
+                        },
+                        style: OutlinedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                        ),
+                        icon: SvgPicture.asset(
+                          'assets/icons/google_logo.svg',
+                          height: 24,
+                        ),
+                        label: const Text('Sign-in with Google'),
+                      ),
+                    ),
+                    const SizedBox(height: 26),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ]),
       ),
     );
   }
